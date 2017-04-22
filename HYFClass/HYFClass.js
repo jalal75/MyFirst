7a1ba68ff964b94102ea7466e6f0e22c1d6299f1
Jalal Alnadeem <jalal.alnadeem@gmail.com>
2017-04-22 03:50:33 +0200

    Homework 8

--- HYFClass/HYFClass.js
+++ HYFClass/HYFClass.js
@@ -1,28 +1,24 @@
-// import Student from './Student'
-// import Teacher from './Teacher'
-// export default class HYFClass {
-//   constructor(members){
-//     this.members= members || []
-//     let membersIsArray = this.members instanceof Array
-//     if (!membersIsArray){
-//       throw new Error ('members should be an array')
-//     }
-//   }
-// addTeacher(members) {
-//   this.members.push(new Teacher(members.firstName, members.lastName))
-// }
-// addStudent(members){
-//   this.members.push(new Student(members))
-// }
-// getAllTeacher(){
-// return  this.members.filter(member => member instanceof Teacher)
-// }
-// getAllStudents(){
-// return  this.members.filter(member => member instanceof Student)
-//
-// }
-// }
- function cube(x) {
-  return x * x * x;
+import Student from './Student'
+import Teacher from './Teacher'
+export default class HYFClass {
+  constructor(members){
+    this.members= members || []
+    let membersIsArray = this.members instanceof Array
+    if (!membersIsArray){
+      throw new Error ('members should be an array')
+    }
+  }
+addTeacher(members) {
+  this.members.push(new Teacher(members.firstName, members.lastName))
 }
-export default cube;
+addStudent(members){
+  this.members.push(new Student(members))
+}
+getAllTeacher(){
+return  this.members.filter(member => member instanceof Teacher)
+}
+getAllStudents(){
+return  this.members.filter(member => member instanceof Student)
+
+}
+}
--- HYFClass/index.js
+++ HYFClass/index.js
@@ -1,21 +1,18 @@
-// import HYFClass from './HYFClass';
-// var JS = new HYFClass([])
-// var jsmemberss = [
-//   {firstName: 'Mauro', lastName: 'Mandracchia', type: 'teacher'},
-//     {firstName: 'David', lastName: 'Furlong', type: 'teacher'},
-//     {firstName: 'Ali', lastName: 'Barakat', type: 'teacher'},
-//     {firstName: 'Jad', lastName: 'Kadour', type: 'Student'}
-// ]
-// jsmemberss.forEach(( members )=>{
-//   if(members.type === 'teacher'){
-//     JS.addTeacher( members )
-//   }else {
-//     JS.addStudent( members )
-//
-//   }
-// })
-// console.log('All Teachers ', JS.getAllTeacher())
-// console.log('All Students ', JS.getAllStudents())
+import HYFClass from './HYFClass';
+var JS = new HYFClass([])
+var jsmemberss = [
+  {firstName: 'Mauro', lastName: 'Mandracchia', type: 'teacher'},
+    {firstName: 'David', lastName: 'Furlong', type: 'teacher'},
+    {firstName: 'Ali', lastName: 'Barakat', type: 'teacher'},
+    {firstName: 'Jad', lastName: 'Kadour', type: 'Student'}
+]
+jsmemberss.forEach(( members )=>{
+  if(members.type === 'teacher'){
+    JS.addTeacher( members )
+  }else {
+    JS.addStudent( members )
 
-import cube from './HYFClass.js';
-console.log(cube(3)); // 27
+  }
+})
+console.log('All Teachers ', JS.getAllTeacher())
+console.log('All Students ', JS.getAllStudents())
